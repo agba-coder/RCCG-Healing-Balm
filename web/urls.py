7bd50cc3c9fff82e8585a0153110b61bb5d549b6
agba-coder <demiladebamgboye@gmail.com>
2023-07-29 13:26:47 +0100

    Add about, contact, prayer-request pages

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name="home"),
+    path('about-us/', views.about, name="about"),
+    path('contact-us/', views.contact, name="contact"),
+    path('prayer-requests/', views.prayer_request, name="prayer_request"),
     # path('favicon.ico', lambda _ : redirect('static/assets/img/favicon.ico', permanent=True)),
 ]
--- web/views.py
+++ web/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def prayer_request(request):
+    return render(request, "prayer-request.html")
